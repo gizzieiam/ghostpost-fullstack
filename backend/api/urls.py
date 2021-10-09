799400bf9cc924e70f29175af8c455ed6d9ac26a
Shavonne Carson <59718290+gizzieiam@users.noreply.github.com>
2021-10-09 12:23:34 -0700

    api urls

---
+++ backend/api/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+from rest_framework import routers
+from api.views import PostViewSet
+
+router = routers.DefaultRouter()
+
+router.register(r'Post', PostViewSet)
+
+urlpatterns = [
+    url(r'^api/', include(router.urls))
+]
