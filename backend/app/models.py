72470481bde9389c226b84b88b53cb62fa04f10d
Shavonne Carson <59718290+gizzieiam@users.noreply.github.com>
2021-10-09 12:11:57 -0700

    created post model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    header = models.CharField(max_length=30)
+    text = models.TextField()
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    create_time = models.DateTimeField(default=timezone.now)
+    roast = models.BooleanField(default=True)
+
+    @property
+    def voteCount(self):
+        return self.likes - self.dislikes
+    
+    def __str__(self):
+        return self.header
