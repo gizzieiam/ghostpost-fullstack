645dd5cb838bdf8adf81a7d8acbb0a67e2c3f149
Shavonne Carson <59718290+gizzieiam@users.noreply.github.com>
2021-10-09 12:22:30 -0700

    api view actions for boast, likes, dislikes, highest rated

--- backend/api/views.py
+++ backend/api/views.py
@@ -13,3 +13,30 @@ class PostViewSet(ModelViewSet):
         getRoasts = Post.objects.filter(roast=True).order_by('-create_time')
         serializer = self.get_serializer(getRoasts, many=True)
         return Response(serializer.data)
+
+    @action(detail=False)
+    def boast(self, request, pk=None):
+        getBoast = Post.objects.filter(roast=False).order_by('-create_time')
+        serializer = self.get_serializer(getBoast, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['post'])
+    def likes(self, request, pk=None):
+        post = Post.objects.get(id=pk)
+        post.likes += 1
+        post.save()
+        return Response({'status': '+1 likes'})
+
+    @action(detail=True, methods=['post'])
+    def dislikes(self, request, pk=None):
+        post = Post.objects.get(id=pk)
+        post.dislikes += 1
+        post.save()
+        return Response({'status': '+1 dislikes'})
+
+    @action(detail=False)
+    def highest(self, request, pk=None):
+        posts = Post.objects.all()
+        sort = sorted(posts, key=lambda a: a.voteCount, reverse=True)
+        serializer = self.get_serializer(sort, many=True)
+        return Response(serializer.data)
