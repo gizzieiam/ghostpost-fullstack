f003c0b1474d06d93750baa55cce6a5cdc8c06e1
Shavonne Carson <59718290+gizzieiam@users.noreply.github.com>
2021-10-09 12:19:57 -0700

    can view roast type post in admin api view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from app.models import Post
+from api import serializers
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+class PostViewSet(ModelViewSet):
+    serializer_class = serializers.PostSerializer
+    queryset = Post.objects.all()
+
+    @action(detail=False)
+    def roast(self, request, pk=None):
+        getRoasts = Post.objects.filter(roast=True).order_by('-create_time')
+        serializer = self.get_serializer(getRoasts, many=True)
+        return Response(serializer.data)
