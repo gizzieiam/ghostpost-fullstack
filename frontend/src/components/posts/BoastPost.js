78a6f86f4fdc643be368f767e6d94fdf37b57d54
Shavonne Carson <59718290+gizzieiam@users.noreply.github.com>
2021-10-10 14:18:40 -0700

    can now view boast post

---
+++ frontend/src/components/posts/BoastPost.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+class BoastPost extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            posts : []
+        }
+    }
+    componentDidMount(){
+        fetch("http://localhost:8000/api/Post/boast/")
+        .then((res)=> res.json())
+        .then((data)=> this.setState({posts:data}))
+    }
+    render(){
+        return (
+            <div id='post-ul'>
+                {this.state.posts.map((post)=>(
+                    <ul key={post.id} className=' mx-width card border-primary mb-3'>
+                        <li id='post-li'>{post.text}</li>
+
+                        <div id='btn-container'>
+                            <button id='likes-btn' className='btn btn-outline-success' onClick={() => {this.handleLikes(post.id)}}>
+                                {post.likes} Likes
+                            </button>
+                            <button id='dislikes-btn'className='btn btn-outline-danger' onClick={() => {this.handleDislikes(post.id)}}>
+                                {post.dislikes} Dislike
+                            </button>
+                        </div>
+
+                        <div id='post-footer'>
+                            <li className='text-muted'>{post.roast ? 'Roast' : 'Boast'}</li>
+                            <li className='text-muted'>Created: {post.create_time.substring(0, 10)}</li>
+                        </div>
+                    </ul>
+                ))}
+            </div>
+        );
+    }
+}
+export default BoastPost
--- frontend/src/routes/Boast.js
+++ frontend/src/routes/Boast.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-
+import BoastPost from '../components/posts/BoastPost.js'
 class Boast extends Component {
     render(){
         return(
             <>
+                <BoastPost />
             </>
         )
     }
